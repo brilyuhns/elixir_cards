f73ecb811eebcce398833e08f3aba23a7975ce1d
Divya Kunnath <kunnathdiyval@gmail.com>
2017-08-27 19:39:44 +0800

    Section 3 of elixir course

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,12 @@
 defmodule Cards do
-  def create_deck do 
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
+  def create_deck do     
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
     for suit <- suits, value <- values do 
@@ -11,10 +18,33 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+  
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do 
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should 
+    be in the hand.
+ 
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do 
     Enum.split(deck, hand_size)
   end
